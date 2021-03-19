0d30fef9ae63989731ac71e69910b092a21acbb4
dankhank8 <80941100+dankhank8@users.noreply.github.com>
2021-03-18 23:08:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,17 +22,27 @@ int main()
         int nextCheckpointDist; // distance to the next checkpoint
         int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
         cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        int thrust = 0;
         int opponentX;
         int opponentY;
         cin >> opponentX >> opponentY; cin.ignore();
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
-
+        if (abs(nextCheckpointAngle) < 5 && nextCheckpointDist > 4000){
+            cout << nextCheckpointX << " " << nextCheckpointY << " " << "BOOST" << endl;
+        } else {
+            if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90) {
+                thrust = 10;
+            } else {
+                thrust = 100;
+            } 
+        cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
+        }  
 
         // You have to output the target position
         // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " 80" << endl;
+        
     }
 }
